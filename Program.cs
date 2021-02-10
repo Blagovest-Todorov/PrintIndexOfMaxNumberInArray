91a4332b7207e29d661cf3e49663244358072dc2
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-10 18:25:57 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayMaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrNum = {5,6,10,70,25,-1,0,10, -100, 100};
+            int MaxNumber = arrNum[0]; //let assume that first number in the array is max/
+            int indexMaxNum = 0;
+
+            for (int i = 0; i < arrNum.Length; i++)
+            {
+                int currNumber = arrNum[i];
+
+                for (int j = i+1; j < arrNum.Length; j++)
+                {
+                    if ((currNumber < arrNum[j]) && (arrNum[j] > MaxNumber))
+                    {
+                        MaxNumber = arrNum[j];
+                        indexMaxNum = j;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{MaxNumber}");
+            Console.WriteLine($"{indexMaxNum}");
+        }
+    }
+}
